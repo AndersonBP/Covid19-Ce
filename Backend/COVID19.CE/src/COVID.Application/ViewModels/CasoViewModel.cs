43363cdca90a8c1414ae3af712ce176cae94d84e
Flábio Filho <ext.flabio.filho@farfetch.com>
2020-03-22 20:55:21 +0000

    Criado nova API (AlternativeAPI) na versão 3.0 do .Net Core

--- Backend/COVID19.CE/src/COVID.Application/ViewModels/CasoViewModel.cs
+++ Backend/COVID19.CE/src/COVID.Application/ViewModels/CasoViewModel.cs
@@ -8,7 +8,7 @@ namespace COVID.Application.ViewModels
 
     public string UF { get; set; }
     public int Cidade { get; set; }
-    public string Bairro { get; set; }
+    public int? Bairro { get; set; }
     public string Sexo { get; set; }
     public int? FaixaEtaria { get; set; }
     public int? Suspeito { get; set; }
--- Backend/COVID19.CE/src/COVID.Domain/Entities/Caso.cs
+++ Backend/COVID19.CE/src/COVID.Domain/Entities/Caso.cs
@@ -11,7 +11,7 @@ namespace COVID.Domain.Entities
     //public int CidadeId { get; set; }
     //public virtual Cidade Cidade { get; set; }
 
-    public string Bairro { get; set; }
+    public int? Bairro { get; set; }
     public string Sexo { get; set; }
 
     public int? FaixaEtaria { get; set; }
