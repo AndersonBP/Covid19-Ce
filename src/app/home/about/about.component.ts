26cb9319f8d7a9171649818700d15be0052a6711
Flábio Filho <ext.flabio.filho@farfetch.com>
2020-03-29 16:29:35 +0100

    ajuste tela: Sobre

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  breakpoint: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 500) ? 1 : ( (window.innerWidth <= 1200)? 2 : 5 );
+  }
+  
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 500) ? 1 : ( (event.target.innerWidth <= 1200)? 2 : 5 );
   }
 
 }
